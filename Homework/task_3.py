bc0c3ff11155fd83fea931d31ed1606e1f9fbde3
hamster108 <saliva108@gmail.com>
2016-02-27 11:58:35 +0300

    changed

--- Homework/task_3.py
+++ Homework/task_3.py
@@ -1,19 +1,20 @@
 class fibonacci_sequence:
     def __init__(self, number):
         self.number = number
-        self.lst = []
-        self.first_digit = 1
+        self.first_digit = 0
         self.second_digit = 1
+        self.count = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if len(self.lst) != self.number:
+        if self.count < self.number:
             remember_digit = self.second_digit
-            self.second_digit = self.first_digit + self.second_digit
-            self.lst.append(self.first_digit)
+            self.second_digit = self.first_digit + self.second_digit           
             self.first_digit = remember_digit
-            return self.lst[-1]
+            self.count += 1
+            return remember_digit
+            
         else:
             raise StopIteration
