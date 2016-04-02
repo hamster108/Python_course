4566675a411a73d21b29bf425e24756e36895295
hamster108 <saliva108@gmail.com>
2016-04-02 13:48:28 +0300

    finally_coorected

--- spring_comes1.py
+++ spring_comes1.py
@@ -3,10 +3,7 @@ import argparse
 import os.path
 import os
 import subprocess
-##def change_image(x, y):
-##    if os.path.isfile(x):
-##        result = 'convert '+ str(x) + ' -resize ' + str(y) + '%'
-##        print(subprocess.call(result, shell=True))
+
     
 parser = argparse.ArgumentParser(description="Changes the picture's size ")
 parser.add_argument("percent", type = int,
@@ -26,11 +23,12 @@ if args.percent:
         result += str(args.way)
         print(subprocess.call(result, shell=True))
     elif os.path.isdir(args.way):
-        for dir, dubdirs, files in os.walk(args.way):
+        for root, dirs, files in os.walk(args.way):
             for name in files:
                 filename = os.path.join(root, name)
-                result = 'convert '+ filename + ' -resize ' + str(args.percent) + '%' + filename + '%'
-
+                result = 'convert '+ filename + ' -resize ' + str(args.percent) + '% ' + filename
+                print(result)
+                print(subprocess.call(result, shell=True))
     
     else:
         print('There is no image')
